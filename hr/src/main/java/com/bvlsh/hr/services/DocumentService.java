fd282fb47d889313c02f2e5febed71503f057991
lorela.shehu <lorela.shehu@DTI-PROGRAM-25.ASP.gov.al>
2019-04-24 11:31:46 +0200

    ui-emplyeeDetails

--- hr/src/main/java/com/bvlsh/hr/services/DocumentService.java
+++ hr/src/main/java/com/bvlsh/hr/services/DocumentService.java
@@ -1,8 +1,34 @@
 package com.bvlsh.hr.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.bvlsh.hr.dao.CrudDAO;
+import com.bvlsh.hr.entities.Document;
+import com.bvlsh.hr.exceptions.ValidationException;
+import com.bvlsh.hr.forms.DocumentForm;
+import com.bvlsh.hr.utils.StringUtil;
 
 @Service
 public class DocumentService {
 
+	@Autowired CrudDAO crudDAO;
+
+	public Document registerDocument(DocumentForm form, String uname) {
+		
+		if (form == null) {
+			throw new ValidationException("Forma e pa plotësuar");
+		}
+		if (!StringUtil.isValid(form.getDocumentName())) {
+			throw new ValidationException("Plotësoni Emrin e Dokumentit !");
+		}
+
+		if (!StringUtil.isValid(form.getDescription())) {
+			throw new ValidationException("Plotësoni Përshkrimin !");
+		}
+		
+		
+			return null;
+		
+	}
 }
