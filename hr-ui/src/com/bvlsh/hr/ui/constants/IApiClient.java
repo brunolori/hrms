aba7e9b3147f091792f70bc10449525e5a6692ad
lorela.shehu <lorela.shehu@DTI-PROGRAM-25.ASP.gov.al>
2019-05-02 00:53:38 +0200

    rps

--- hr-ui/src/com/bvlsh/hr/ui/constants/IApiClient.java
+++ hr-ui/src/com/bvlsh/hr/ui/constants/IApiClient.java
@@ -4,6 +4,6 @@ public interface IApiClient {
 	
 	
 	public static final String SERVER = "http://localhost:8888/hr";
-	public static final String NCR_API = "http://rps.asp.gov.al:8080/";
+	public static final String NCR_API = "http://10.10.22.14/";
 
 }
